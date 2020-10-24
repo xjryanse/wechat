9839bfce4ae0c43ff755edb283bcf61206e141ec
xieJialun <36151536+xjryanse@users.noreply.github.com>
2020-10-24 10:05:51 +0800

    token记录

--- src/service/WechatWePubOauthAccessTokenService.php
+++ src/service/WechatWePubOauthAccessTokenService.php
@@ -14,4 +14,26 @@ class WechatWePubOauthAccessTokenService implements MainModelInterface
     protected static $mainModel;
     protected static $mainModelClass    = '\\xjryanse\\wechat\\model\\WechatWePubOauthAccessToken';
 
+    /**
+     * token保存
+     * @param type $data
+     * @return type
+     */
+    public static function tokenSave($data)
+    {
+        if(!isset($data['openid']) || !$data['openid']){
+            return false;
+        }
+        $times = time() + $data['expires_in'];
+        $data['expires_time'] = date('Y-m-d H:i:s',$times);
+        //已有记录更新，没有记录新增
+        $con[]  = ['openid','=',$data['openid']];
+        $res = self::find( $con );
+        if($res){
+            $data['id'] = $res['id'];
+            return self::update($data);
+        } else {
+            return self::save($data);
+        }
+    }    
 }
