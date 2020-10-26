1c70bae7f8925651bbb0d47f73ac16fc81d15d68
xieJialun <36151536+xjryanse@users.noreply.github.com>
2020-10-26 12:02:35 +0800

    微信支付官方类库改命名空间

---
+++ src/WxPay/lib/WxPayNotifyResults.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * 2015-06-29 修复签名问题
+ * */
+
+namespace xjryanse\wechat\WxPay\lib;
+
+/**
+ *
+ * 回调回包数据基类
+ *
+ * */
+class WxPayNotifyResults extends WxPayResults {
+
+    /**
+     * 将xml转为array
+     * @param WxPayConfigInterface $config
+     * @param string $xml
+     * @return WxPayNotifyResults
+     * @throws WxPayException
+     */
+    public static function Init($config, $xml) {
+        $obj = new self();
+        $obj->FromXml($xml);
+        //失败则直接返回失败
+        $obj->CheckSign($config);
+        return $obj;
+    }
+
+}
