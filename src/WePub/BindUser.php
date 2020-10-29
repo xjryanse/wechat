689b614faf3600b6987f1d626f25a4a12ba7a3f6
xieJialun <36151536+xjryanse@users.noreply.github.com>
2020-10-29 15:32:37 +0800

    改绑

--- src/WePub/BindUser.php
+++ src/WePub/BindUser.php
@@ -10,8 +10,12 @@ class BindUser
 {
     /**
      * 获取绑定的用户id
+     * @param type $openid      openid
+     * @param type $scene       场景值
+     * @param type $emptyCreate 无用户是否创建
+     * @return type
      */
-    public static function getBindUserId( $openid, $scene="default", $emptyCreate = false  )
+    public static function getBindUserId( $openid, $scene="", $emptyCreate = false  )
     {
         $con    = [];
         $con[]  = ['openid','=',$openid];
@@ -27,5 +31,40 @@ class BindUser
         }
 
         return $info ? $info['user_id'] : '';
+    }
+    /**
+     * 用户改绑
+     * @param type $openid
+     * @param type $userId
+     * @param type $scene
+     * @param type $deleteIfEmpty   若先前的用户为空，是否删除？默认不删
+     */
+    public static function changeBind( $openid, $userId, $scene= "", $deleteIfEmpty = false)
+    {
+        $info = self::getBindUserId($openid, $scene);
+        if($info && $info['user_id'] == $userId){
+            //相同用户不操作
+            return false;
+        }
+        $userInfo = UserService::getInstance($info['user_id'])->get();
+        //如果为空,且强制删用户，则删
+        if($info && $deleteIfEmpty && !$userInfo['username']){
+            UserService::getInstance($info['user_id'])->delete();
+        }
+        //绑定信息
+        $con    = [];
+        $con[]  = ['openid','=',$openid];
+        $con[]  = ['scene','=',$scene];
+        $bind = WechatWePubFansUserService::find( $con );
+        if( $bind ){
+            //更新绑定记录
+            return WechatWePubFansUserService::getInstance($bind['id'])->update(['user_id'=>$userId]);
+        } else {
+            //新增绑定记录
+            $data['openid']     = $openid;
+            $data['scene']      = $scene;
+            $data['user_id']    = $userInfo['id'];
+            $info = WechatWePubFansUserService::save( $data );
+        }
     }
 }
